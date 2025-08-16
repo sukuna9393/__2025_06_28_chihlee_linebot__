306abec84d7e6f276603c49fba1c98a7299c25aa
roberthsu2003 <roberthsu2003@gmail.com>
2025-08-16 10:52:11 +0800

    modify

--- lesson8/lesson8_1.py
+++ lesson8/lesson8_1.py
@@ -3,10 +3,10 @@ import gradio as gr
 def greet(name, intensity):
     return  name + "您好" + "!" * int(intensity)
 
-demo = gr.Interface(
-    fn=greet,
+demo = gr.Interface(    
     inputs=["text", "slider"],
     outputs=["text"],
+    fn=greet
 )
 
 demo.launch(share=True)
