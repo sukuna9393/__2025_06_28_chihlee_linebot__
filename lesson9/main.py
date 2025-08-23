5946fb581fb2436044e830091e82361a320b02c7
roberthsu2003 <roberthsu2003@gmail.com>
2025-08-23 10:57:23 +0800

    modify

--- lesson9/main.py
+++ lesson9/main.py
@@ -3,5 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return "<p>Hello, Flask!</p>"
+
+@app.route("/name")
+def my_name():
+    return "<h1>Robert</h1>"
